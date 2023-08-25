24dad20bed9abdde447c869ee5c61614b375e765
aniketkoli <aniketkoli17884@gmail.com>
2023-08-25 15:22:34 +0000

    iplDashBoard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
---
+++ src/components/Home/index.js
@@ -0,0 +1,61 @@
+// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard'
+import './index.css'
+
+class Home extends Component {
+  state = {matchData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getMatchData()
+  }
+
+  getMatchData = async () => {
+    const {matchData} = this.state
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+    console.log(data)
+    const formattedData = data.teams.map(eachItem => ({
+      id: eachItem.id,
+      name: eachItem.name,
+      teamImageUrl: eachItem.team_image_url,
+    }))
+    this.setState({matchData: formattedData, isLoading: false})
+    console.log(matchData)
+  }
+
+  renderTeamsList = () => {
+    const {matchData} = this.state
+    return (
+      <ul className="teams-list-items">
+        {matchData.map(team => (
+          <TeamCard key={team.id} teamData={team} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderLoader = () => {
+    ;<div data-testid="loader" className="loader-container">
+      <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+    </div>
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="bg-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+          alt="ipl logo"
+          className="logo"
+        />
+        <h1 className="main-heading">IPL DashBoard</h1>
+        {isLoading ? this.renderLoader() : this.renderTeamsList()}
+      </div>
+    )
+  }
+}
+
+export default Home
