c46258c40d3a2a2357c400b6795e51b17ff4861f
Aniket Navanath Koli <134407019+ANIKETKOLI20@users.noreply.github.com>
2023-08-27 12:49:54 +0530

    Update index.js

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,1 +1,19 @@
 // Write your code here
+import {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './index.css'
+
+class TeamCard extends Component {
+  render() {
+    const {teamData} = this.props
+    const {id, name, teamImageUrl} = teamData
+    return (
+      <Link to={`/team-matches/${id}`} className="link-item">
+        <li className="team-card">
+          <img src={teamImageUrl} className="image" alt={`${name}`} />
+          <p className="para">{name}</p>
+        </li>
+      </Link>
+    )
+  }
+}
